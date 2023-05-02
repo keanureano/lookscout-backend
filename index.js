74a99d68f65cbce75e87547867deb8d9e9b8ea1e
Keanu Reaño <reanokeanu@gmail.com>
2023-05-02 22:11:50 +0800

    add routers and views

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const apiRouter = require("./src/controllers/apiRouter");
+const adminRouter = require("./src/controllers/adminRouter");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", adminRouter);
+app.use("/api", apiRouter);
 
 mongoose.connect(process.env.MONGODB_URI);
 const connection = mongoose.connection;
---
+++ src/controllers/adminRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/src/views/admin.html");
+});
+
+module.exports = router;
