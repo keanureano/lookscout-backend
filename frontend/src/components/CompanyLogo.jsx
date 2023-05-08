0ef0837a2f381eaf4a76ea3688450b33c07313c4
Keanu Reaño <reanokeanu@gmail.com>
2023-05-08 15:12:30 +0800

    implement logo

--- frontend/src/components/CompanyLogo.jsx
+++ frontend/src/components/CompanyLogo.jsx
@@ -1,9 +1,10 @@
-import { LogoIcon } from "./Icons";
+import { logoUrl } from "../services/apiService";
+
 function CompanyLogo() {
   return (
     <div className="CompanyLogo">
-      <LogoIcon />
-      Lookscout
+      <img src={logoUrl} width={40} height={40} alt="logo" />
+      <p>Lookscout</p>
     </div>
   );
 }
